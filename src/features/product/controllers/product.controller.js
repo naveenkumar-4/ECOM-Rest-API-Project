dde6aaeb607ee80ad56dac501ec6c9ff1e8100ed
Naveen Kumar <naveenkumarrockz362@gmail.com>
2024-06-11 13:55:38 +0530

    Fixed RatPeroduct  problem

--- src/features/product/controllers/product.controller.js
+++ src/features/product/controllers/product.controller.js
@@ -60,8 +60,8 @@ export default class ProductController {
     console.log(query);
     try {
       const userID = req.userID;
-      const productID = req.query.productID;
-      const rating = req.query.rating;
+      const productID = req.body.productID;
+      const rating = req.body.rating;
       // try{
       //   ProductModel.rateProduct(userID, productID, rating);
       // }catch(err){
--- src/features/product/product.repository.js
+++ src/features/product/product.repository.js
@@ -65,11 +65,64 @@ export default class ProductRepository {
     }
   }
 
+  // async rateProducts(userID, productID, rating) {
+  //   try {
+  //     const db = getDB();
+  //     const collection = db.collection(this.collection);
+  //     // 1. Find the product
+  //     const product = await collection.findOne({
+  //       _id: new ObjectId(productID),
+  //     });
+  //     // 2. Find the rating
+  //     const userRating = product?.ratings?.find(
+  //       (rating) => rating.userID == userID
+  //     );
+  //     if (userRating) {
+  //       // 3. Update the existing rating
+  //       await collection.updateOne(
+  //         {
+  //           _id: new ObjectId(productID),
+  //           "ratings.userID": new ObjectId(userID),
+  //         },
+  //         {
+  //           // the "$.rating" is a placeholder which is the first data i.e first rating
+  //           // that was given based on the above condition
+  //           $set:{
+  //             "ratings.$.rating":rating
+  //           }
+  //         }
+  //       );
+  //     } else {
+  //       await collection.updateOne(
+  //         {
+  //           _id: new ObjectId(productID),
+  //         },
+  //         {
+  //           $push: { ratings: { userID: new ObjectId(userID), rating } },
+  //         }
+  //       );
+  //     }
+  //   } catch (err) {
+  //     console.log(err);
+  //     throw new ApplicationHandler("Something Went Wrong with dataBase", 500);
+  //   }
+  // }
+
   async rateProducts(userID, productID, rating) {
     try {
       const db = getDB();
       const collection = db.collection(this.collection);
-      collection.updateOne(
+      // 1.removes existing entry
+      await collection.updateOne(
+        {
+          _id: new ObjectId(productID),
+        },
+        {
+          $pull: { ratings: { userID: new ObjectId(userID) } },
+        }
+      );
+      // 2. Add new entry
+      await collection.updateOne(
         {
           _id: new ObjectId(productID),
         },
